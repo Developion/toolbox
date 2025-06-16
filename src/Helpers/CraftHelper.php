2c6fc19eb6d182b2a8111fca16bff801d29759a5
Marko Gajić <metalmorgoth@gmail.com>
2025-06-16 04:32:59 +0000

    fix alias

--- src/Helpers/CraftHelper.php
+++ src/Helpers/CraftHelper.php
@@ -45,7 +45,7 @@ class CraftHelper
 
 		$path = Craft::getAlias('@config/' . $fileName, false);
 		if ($path === false || !file_exists($path)) {
-			$path = Craft::getAlias('@nystudio107/codeeditor/config.php', false);
+			$path = Craft::getAlias('@developion/toolbox/config.php', false);
 			if ($path === false || !file_exists($path)) {
 				return [];
 			}
